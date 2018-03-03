b171f26081ec2e39d42a8ffdb84dfe7da49286ad
chrisdias <cdias@microsoft.com>
2018-03-02 16:50:09 -0800

    demo

--- src/extension.ts
+++ src/extension.ts
@@ -5,15 +5,22 @@ import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
 
-    vscode.window.showInformationMessage('I just wanted to let you know I am still here', 'Please call me');
-    vscode.window.showInformationMessage('Hello from the other side', 'Buy Now', 'Listen on Spotify');
+
+    await vscode.window.showInformationMessage('Hello There!', 'Hi');
+    await vscode.window.showInformationMessage('This extension has been updated, read the README?', 'Yes', 'No', 'Later');
+    await vscode.window.showInformationMessage('C\'mon, read it, it is very important', 'OK', 'OK');
     vscode.window.showInformationMessage('VS Code has been updated, do you want to restart to install?', 'Heck Yeah!');
     vscode.window.showInformationMessage('We recommend you install the C# extension!', 'Show Recommendations', 'Never Show Again');
-    vscode.window.showInformationMessage('Are you sure you don\'t want to read the read me?', 'OK', 'No');
-    vscode.window.showInformationMessage('Notifications Nightmare Readme has been updated, do you want to read it?', 'Yes', 'No', 'Later');
-    vscode.window.showInformationMessage('Hello There!!!','Hi','There');
+    vscode.window.showInformationMessage('We recommend you install the PHP extension!', 'Show Recommendations', 'Never Show Again');
+    vscode.window.showInformationMessage('We recommend you install the C++ extension!', 'Show Recommendations', 'Never Show Again');
+    vscode.window.showInformationMessage('We recommend you install the Ruby extension!', 'Show Recommendations', 'Never Show Again');
+    setTimeout(() => {
+        vscode.window.showInformationMessage('I just wanted to let you know I am still here', 'Call me');
+    }, 10000);
+
+
 
 }
 
